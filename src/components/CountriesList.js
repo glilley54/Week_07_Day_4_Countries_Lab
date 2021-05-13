ba16b3cb1ce56283d3fffae943c1baf272304d83
glilley54 <glilley54@gmail.com>
2021-05-13 11:20:04 +0100

    countries list compiled

---
+++ src/components/CountriesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CountryItem from './CountryItem';
+
+const CountriesList = ({countries}) => {
+
+    const countryNodes = countries.map((country, index) => {
+        return <CountryItem country = {country} key = {index}>{country.name}</CountryItem>
+    })
+
+    return (
+     <ul>
+         {countryNodes}
+     </ul>
+
+    )
+}
+
+export default CountryItem;
